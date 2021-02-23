1cbf10fe5bba48a22624b45b0347d3a091762684
Cinthia <cinthia.tengan@gmail.com>
2021-02-23 16:08:55 -0300

    added linkedhashset class

--- Java-collections/src/main/java/JavaList/exemploLinkedHashSet.java
+++ Java-collections/src/main/java/JavaList/exemploLinkedHashSet.java
@@ -1,7 +1,40 @@
 package JavaList;
 
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
 public class exemploLinkedHashSet {
     public static void main(String[] args) {
-        
+        LinkedHashSet<Integer> sequenciaNumerica = new LinkedHashSet<>();
+
+        //adiciona os numeros no set
+        sequenciaNumerica.add(1);
+        sequenciaNumerica.add(2);
+        sequenciaNumerica.add(4);
+        sequenciaNumerica.add(8);
+        sequenciaNumerica.add(16);
+
+        System.out.println(sequenciaNumerica);
+
+        //remove o numero do set
+        sequenciaNumerica.remove(4);
+        System.out.println(sequenciaNumerica);
+
+        //retorna a quantidade de itens do set
+        System.out.println(sequenciaNumerica.size());
+
+        //navega em todos os itens do iterator
+        Iterator<Integer> iterator = sequenciaNumerica.iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        for (Integer numero: sequenciaNumerica){
+            System.out.println(numero);
+        }
+
+        //retorna se o set esta vario ou não
+        System.out.println(sequenciaNumerica.isEmpty());
     }
+
 }
