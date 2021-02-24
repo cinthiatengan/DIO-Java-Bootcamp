2f3b6439644109de4e72b4f3c39e49cc6e39d623
Cinthia <cinthia.tengan@gmail.com>
2021-02-24 18:31:07 -0300

    java optional class 3

---
+++ Java-collections/src/main/java/JavaList/exemploOptional3.java
@@ -0,0 +1,21 @@
+package JavaList;
+
+import java.util.Optional;
+
+public class exemploOptional3 {
+    public static void main(String[] args) {
+        Optional<String> optionalString = Optional.of("Valor opcional");
+
+        System.out.println(optionalString.isPresent());
+
+        optionalString.ifPresent(System.out::println);
+        optionalString.ifPresentOrElse(System.out::println, () -> System.out.println("Valor não esta presente"));
+
+        if(optionalString.isPresent()){
+            String valor = optionalString.get();
+            System.out.println(valor);
+        }
+        optionalString.map((valor) -> valor.concat("****")).ifPresent(System.out::println);
+        optionalString.orElseThrow(IllegalStateException::new);
+    }
+}
